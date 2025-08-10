d45980b1a9910d1336eb713b56f545e97cec4b16
watchingx <agragemers77@gmail.com>
2025-08-10 10:10:24 +0700

    gas

--- next.config.ts
+++ next.config.ts
@@ -17,8 +17,8 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
               "img-src 'self' data: https:;",
               "font-src 'self' data: https://fonts.gstatic.com https://cdn.jsdelivr.net;",
-              "frame-src 'self' https://www.google.com/recaptcha/;",
-              "connect-src 'self';",
+              "frame-src 'self' https://www.google.com https://www.gstatic.com;",
+              "connect-src 'self' https://www.google.com https://www.gstatic.com;",
             ].join(" "),
           },
           {
